e720c8a3e4075581497a4296ae2533df45c40805
RKPH <pnghung2003@gmail.com>
2024-06-27 09:49:15 +0700

    khoi tao folder Medium

--- Easy/242. Valid Anagram/242. Valid Anagram/Program.cs
+++ Easy/242. Valid Anagram/242. Valid Anagram/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+public class Solution {
+    public bool IsAnagram(string s, string t)
+    {
+        return true;
+    }
+}
 
-Console.WriteLine("Hello hung, World!");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Hello, World!");
+    }
+}          
