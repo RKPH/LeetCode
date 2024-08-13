369a57571f4c21c03a655d504cc83bb4bcf96b13
RKPH <pnghung2003@gmail.com>
2024-08-14 00:34:18 +0700

    initalize and solve medium#3

---
+++ Medium/3. Longest Substring Without Repeating Characters/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace DefaultNamespace;
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s) { //method 1
+        int start = 0;
+        int maxLength = 0;
+        string result = "";
+
+        while (start < s.Length) {
+            int pointer = start;
+            while (pointer < s.Length && !result.Contains(s[pointer])) {
+                result += s[pointer];
+                pointer++;
+            }
+            maxLength = Math.Max(maxLength, result.Length);
+            result = "";
+            start++;
+        }
+
+        return maxLength;
+    }
+}
